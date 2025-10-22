9935cd2c32b2d7c0d5eb00d3834efa03b887f3c7
Adel Ahmed <adel15952@gmail.com>
2025-10-22 17:22:33 +0300

    Add AppColors and update AppText to use color constants

--- src/components/texts/AppText.tsx
+++ src/components/texts/AppText.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, TextProps, TextStyle } from 'react-native';
 import { s } from 'react-native-size-matters';
 import { ReactNode, FC } from 'react';
+import { AppColors } from '../../styles/colors';
 
 interface AppTextProps extends TextProps {
   children: ReactNode;
@@ -23,10 +24,10 @@ export default AppText;
 const styles = StyleSheet.create({
   bold: {
     fontSize: s(18),
-    color: '#000',
+    color: AppColors.black,
   },
   medium: {
     fontSize: s(16),
-    color: '#000',
+    color: AppColors.black,
   },
 });
---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export const AppColors = {
+  primary: '#1E88E5',
+  secondary: '#FFC107',
+  white: '#FFFFFF',
+  black: '#121212',
+  lightGray: '#F5F5F5',
+  background: '#FAFAFA',
+  disabledGray: '#BDBDBD',
+  blueGray: '#90A4AE',
+  medGray: '#757575',
+  success: '#4CAF50',
+  error: '#E53935',
+};
