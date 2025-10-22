3913870d387e2832ae70aa7b397745409dbdd25c
Adel Ahmed <adel15952@gmail.com>
2025-10-22 17:36:48 +0300

    Add AppSafeView component and update App to use it

---
+++ src/components/views/AppSafeView.tsx
@@ -0,0 +1,29 @@
+import { StatusBar, StyleSheet, View, ViewStyle } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AppColors } from '../../styles/colors';
+import { ReactNode, FC } from 'react';
+import { IS_ANDROID } from '../../constants/constants';
+
+interface AppSafeViewProps {
+  children: ReactNode;
+  style?: ViewStyle;
+}
+
+const AppSafeView: FC<AppSafeViewProps> = ({ children, style }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, style]}>{children}</View>
+    </SafeAreaView>
+  );
+};
+export default AppSafeView;
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: AppColors.white,
+    paddingTop: IS_ANDROID ? StatusBar.currentHeight || 0 : 0,
+  },
+  container: {
+    flex: 1,
+  },
+});
