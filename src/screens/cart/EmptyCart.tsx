5a237f5c6dae7475d58e18b43ebdd2633f1cecb9
Adel Ahmed <adel15952@gmail.com>
2025-10-23 16:26:17 +0300

    Add EmptyCart component to CartScreen for handling empty cart state

---
+++ src/screens/cart/EmptyCart.tsx
@@ -0,0 +1,63 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+import AppText from '../../components/texts/AppText';
+import { s, vs } from 'react-native-size-matters';
+import { AppFonts } from '../../styles/fonts';
+import { AppColors } from '../../styles/colors';
+import AppButton from '../../components/buttons/AppButton';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const EmptyCart = () => {
+  const navigation = useNavigation<NavigationProp<any>>();
+  return (
+    <View style={styles.container}>
+      <MaterialCommunityIcons
+        name="shopping-outline"
+        size={s(100)}
+        color={AppColors.primary}
+        style={styles.icon}
+      />
+      <AppText style={styles.title}>Your cart is empty</AppText>
+      <AppText style={styles.subTitle}>
+        Browse our products and add some items to your cart.
+      </AppText>
+      <AppButton
+        title="Start Shopping"
+        style={styles.button}
+        onPress={() => navigation.navigate('Home')}
+      />
+    </View>
+  );
+};
+
+export default EmptyCart;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: s(20),
+  },
+  title: {
+    fontSize: s(24),
+    fontFamily: AppFonts.bold,
+    color: AppColors.primary,
+    marginBottom: vs(10),
+  },
+  subTitle: {
+    fontSize: s(16),
+    fontFamily: AppFonts.medium,
+    color: AppColors.medGray,
+    textAlign: 'center',
+    marginBottom: vs(20),
+  },
+  button: {
+    width: '80%',
+  },
+  icon: {
+    marginBottom: vs(20),
+    opacity: 0.9,
+  },
+});
