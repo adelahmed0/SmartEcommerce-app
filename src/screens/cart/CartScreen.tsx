c856fe43ad8f1d235853c175f3ac6290ee709edf
Adel Ahmed <adel15952@gmail.com>
2025-10-24 21:45:00 +0300

    Refactor CartScreen to calculate and display total prices, including taxes and shipping fees. Implement conditional rendering for cart items and empty cart state.

--- src/screens/cart/CartScreen.tsx
+++ src/screens/cart/CartScreen.tsx
@@ -11,6 +11,7 @@ import AppButton from '../../components/buttons/AppButton';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
+import { TAXES, SHIPPING_FEE } from '../../constants/constants';
 import {
   removeItemFromCart,
   removeProductFromCart,
@@ -21,36 +22,45 @@ const CartScreen = () => {
   const navigation = useNavigation<NavigationProp<any>>();
   const { items } = useSelector((state: RootState) => state.cartSlice);
   const dispatch = useDispatch();
+  const totalProductsPriceSum = items.reduce((acc, item) => acc + item.sum, 0);
+  const orderTotal = totalProductsPriceSum + TAXES + SHIPPING_FEE;
+
   return (
     <AppSafeView>
       <HomeHeader />
-      {/* <EmptyCart /> */}
-      <View style={{ paddingHorizontal: sharedPaddingHorizontal, flex: 1 }}>
-        <FlatList
-          data={items}
-          renderItem={({ item }) => (
-            <CartItem
-              {...item}
-              price={item.sum}
-              onIncrementPress={() => {
-                dispatch(addItemToCart(item));
-              }}
-              onDecrementPress={() => {
-                dispatch(removeItemFromCart(item));
-              }}
-              onDeletePress={() => {
-                dispatch(removeProductFromCart(item));
-              }}
-            />
-          )}
-          keyExtractor={(item) => item.id.toString()}
-        />
-        <TotalView itemsPrice={100} orderTotal={100} />
-        <AppButton
-          title="Continue"
-          onPress={() => navigation.navigate('CheckOutScreen')}
-        />
-      </View>
+      {items.length > 0 ? (
+        <View style={{ paddingHorizontal: sharedPaddingHorizontal, flex: 1 }}>
+          <FlatList
+            data={items}
+            renderItem={({ item }) => (
+              <CartItem
+                {...item}
+                price={item.sum}
+                onIncrementPress={() => {
+                  dispatch(addItemToCart(item));
+                }}
+                onDecrementPress={() => {
+                  dispatch(removeItemFromCart(item));
+                }}
+                onDeletePress={() => {
+                  dispatch(removeProductFromCart(item));
+                }}
+              />
+            )}
+            keyExtractor={(item) => item.id.toString()}
+          />
+          <TotalView
+            itemsPrice={totalProductsPriceSum}
+            orderTotal={orderTotal}
+          />
+          <AppButton
+            title="Continue"
+            onPress={() => navigation.navigate('CheckOutScreen')}
+          />
+        </View>
+      ) : (
+        <EmptyCart />
+      )}
     </AppSafeView>
   );
 };
