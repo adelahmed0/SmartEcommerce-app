6115d747b1f41a9793284259907ac0eb0b293052
Adel Ahmed <adel15952@gmail.com>
2025-10-26 00:08:04 +0300

    Add Firebase configuration and initialization in firebase.ts for app integration

---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-foZ5G0dpDTgGyo3PZyIBg16MRG5Jh8M",
+  authDomain: "smart-ecommerce-app-ef5d2.firebaseapp.com",
+  projectId: "smart-ecommerce-app-ef5d2",
+  storageBucket: "smart-ecommerce-app-ef5d2.firebasestorage.app",
+  messagingSenderId: "375350045195",
+  appId: "1:375350045195:web:41bcec56916222dbe1f50e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
