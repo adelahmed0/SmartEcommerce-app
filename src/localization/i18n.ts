fc423812224db8ca4a9ae3b71a28929faac528e2
Adel Ahmed <adel15952@gmail.com>
2025-10-26 22:26:34 +0300

    Update i18n configuration to include language translations for English and Arabic by importing JSON files.

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import en from './en.json';
+import ar from './ar.json';
 
 const LANGUAGES = {
-  en: {},
-  ar: {},
+  en: { translation: en },
+  ar: { translation: ar },
 };
 
 i18n.use(initReactI18next).init({
