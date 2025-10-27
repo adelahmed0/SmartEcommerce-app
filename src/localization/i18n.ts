68e18c041233374ef299096915821aeaf739e62d
Adel Ahmed <adel15952@gmail.com>
2025-10-27 15:24:49 +0300

    Implement language detection and caching in i18n configuration using AsyncStorage for improved user experience. Refactor initialization to support asynchronous language retrieval and fallback to Arabic if no language is saved.

--- src/localization/i18n.ts
+++ src/localization/i18n.ts
@@ -4,6 +4,7 @@ import en from './en.json';
 import ar from './ar.json';
 import de from './de.json';
 import fr from './fr.json';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LANGUAGES = {
   en: { translation: en },
@@ -12,18 +13,67 @@ const LANGUAGES = {
   fr: { translation: fr },
 };
 
-i18n.use(initReactI18next).init({
-  fallbackLng: 'ar',
-  lng: 'ar', // تعيين اللغة الافتراضية إلى العربية
-  resources: LANGUAGES,
-  defaultNS: 'translation',
-  ns: ['translation'],
-  react: {
-    useSuspense: false,
+const LANGUAGE_DETECTOR = {
+  type: 'languageDetector',
+  async: true,
+  detect: async (callback: (lng: string) => void) => {
+    try {
+      const SavedLanguage = await AsyncStorage.getItem('LANGUAGE');
+      if (SavedLanguage) {
+        callback(SavedLanguage);
+      }
+    } catch (error) {
+      console.error('Error getting language from AsyncStorage:', error);
+      callback('ar');
+    }
+    callback('ar');
   },
-  interpolation: {
-    escapeValue: false,
+  cacheUserLanguage: async (lng: string) => {
+    try {
+      await AsyncStorage.setItem('LANGUAGE', lng);
+    } catch (error) {
+      console.error('Error getting language from AsyncStorage:', error);
+    }
   },
-});
+};
+
+i18n
+  .use({
+    type: 'languageDetector',
+    async: true,
+    detect(callback: (lng: string) => void) {
+      AsyncStorage.getItem('LANGUAGE')
+        .then((SavedLanguage) => {
+          if (SavedLanguage) {
+            callback(SavedLanguage);
+          } else {
+            callback('ar');
+          }
+        })
+        .catch((error) => {
+          console.error('Error getting language from AsyncStorage:', error);
+          callback('ar');
+        });
+    },
+    init() {},
+    cacheUserLanguage(lng: string) {
+      AsyncStorage.setItem('LANGUAGE', lng).catch((error) => {
+        console.error('Error setting language in AsyncStorage:', error);
+      });
+    },
+  })
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'ar',
+    resources: LANGUAGES,
+    defaultNS: 'translation',
+    ns: ['translation'],
+    react: {
+      useSuspense: false,
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
